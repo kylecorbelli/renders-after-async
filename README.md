45d455a5597939c7f92ac282cc92d8337da83ad4
Kyle Corbelli <kyle.corbelli@gmail.com>
2018-07-31 11:10:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# renders-after-async
---
+++ src/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+type ReactComponent<Props>
+  = React.ComponentClass<Props>
+  | React.StatelessComponent<Props>
+
+interface WrapperState {
+  isLoading: boolean
+  error: Error | null
+}
+
+export const rendersAfterAsync = function<Props> (
+  performAsync: (props: Props) => Promise<void>,
+  LoadingComponent: ReactComponent<Props> | null = null,
+  ErrorComponent: ReactComponent<Props> | null = null
+) {
+  return function (Component: ReactComponent<Props>): React.ComponentClass<Props> {
+    return class Wrapper extends React.Component<Props, WrapperState> {
+      public state = {
+        error: null,
+        isLoading: true,
+      }
+
+      public async componentDidMount () {
+        try {
+          await performAsync(this.props)
+          this.setState({ isLoading: false })
+        } catch (error) {
+          this.setState({ error, isLoading: false })
+          throw error
+        }
+      }
+
+      public render () {
+        const { error, isLoading } = this.state
+        if (isLoading) return LoadingComponent ? <LoadingComponent /> : null
+        if (error) return ErrorComponent ? <ErrorComponent /> : null
+        return <Component {...this.props} />
+      }
+    }
+  }
+}
